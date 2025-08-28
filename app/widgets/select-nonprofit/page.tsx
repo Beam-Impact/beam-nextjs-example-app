fbdadb7519cfd75c16b60c1c278dcf7205001a6d
Justin Mastic <justin@beamimpact.com>
2025-08-27 21:59:06 -0600

    Add back event handler example

--- app/widgets/select-nonprofit/page.tsx
+++ app/widgets/select-nonprofit/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { events } from "@beamimpact/web-sdk/dist/integrations/utils";
 import { useBeam } from "@/app/common/beamContext";
+import { useState } from "react";
 import dynamic from "next/dynamic";
 
 const BeamSelectNonprofit = dynamic(
@@ -12,9 +14,23 @@ const BeamSelectNonprofit = dynamic(
 
 export default function Widget() {
   const beamConfig = useBeam();
+  const [selectedNonprofitHistory, setSelectedNonprofit] = useState<string[]>(
+    [],
+  );
+
   return (
     <>
-      <BeamSelectNonprofit {...beamConfig}></BeamSelectNonprofit>
+      <BeamSelectNonprofit
+        {...beamConfig}
+        onNonprofitSelect={(event: events.BeamNonprofitSelectEvent) => {
+          const { selectedNonprofitId, nonprofitName } = event.detail;
+          console.log(event.detail);
+          setSelectedNonprofit([
+            ...selectedNonprofitHistory,
+            nonprofitName || "No Selection",
+          ]);
+        }}
+      ></BeamSelectNonprofit>
     </>
   );
 }
